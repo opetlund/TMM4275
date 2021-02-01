72f3969755197c79ef54f0a2271fd5d5bf7d4b12
Oscar Petlund <opetl@hotmail.com>
2021-02-01 12:58:52 +0100

    POST method added and multiple parameters compatibility.

--- clientServer.py
+++ clientServer.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import re
 
 HOST_NAME = '127.0.0.1'  # locathost - http://127.0.0.1
 # Maybe set this to 1234 / So, complete address would be: http://127.0.0.1:1234
@@ -8,23 +9,65 @@ PORT_NUMBER = 1234
 # Handler of HTTP requests / responses
 
 
+def parse_parameters(string):
+    # Returns list of elements of ["param", "value"]
+    params = string.split("&")
+    for i, val in enumerate(params):
+        params[i] = val.split("=")
+    return params
+
+
+def update_defaults(filename, params):
+    # Sets the default values in html file
+    file = open(filename)
+    data = file.read()
+    file.close()
+    for param in params:
+        find = 'name="' + param[0] + '"' + ' value=".*?"'
+        # name="cdepth" value="123456"><br>
+        replace = 'name="' + param[0] + '"' + ' value="' + param[1] + '"'
+        data = re.sub(find, replace, data)
+    file = open(filename, "wt")
+    file.write(data)
+    file.close()
+
+
 class MyHandler(BaseHTTPRequestHandler):
 
     # Function for displaying html file
-    def write_HTML_file(self, path):
-        self.path = path
+    def write_HTML_file(self, filename):
         try:
-            file = open(self.path[1:]).read()
+            file = open(filename).read()
             self.send_response(200)
+            self.send_header("Content-type", "text/html")
         except:
-            file = "file not found"
+            file = "Error. File not found."
             self.send_response(404)
         self.end_headers()
         self.wfile.write(bytes(file, 'utf-8'))
 
     def do_GET(self):
-        if self.path == '/':
-            self.write_HTML_file("7userinterface.html")
+        if self.path == '/' or self.path == "/orderChair":
+            self.write_HTML_file("userinterface.html")
+
+    def do_POST(self):
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        # Check what is the path
+        path = self.path
+        if path.find("/orderChair") != -1:
+            # Get the paramters
+            content_len = int(self.headers.get('Content-Length'))
+            post_body = self.rfile.read(content_len)
+            param_line = post_body.decode()
+            params = parse_parameters(param_line)
+
+            # html interface to show
+            update_defaults("userinterface2.html", params)
+            self.write_HTML_file("userinterface2.html")
 
 
 if __name__ == '__main__':
