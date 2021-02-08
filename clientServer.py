ed02107b1d88a295529abc07c83c938206909bb9
Oscar Petlund <opetl@hotmail.com>
2021-02-08 13:04:24 +0100

    Parameter interval check

--- clientServer.py
+++ clientServer.py
@@ -6,6 +6,16 @@ HOST_NAME = '127.0.0.1'  # locathost - http://127.0.0.1
 PORT_NUMBER = 1234
 # Web servers example: http://www.ntnu.edu:80
 
+length_min = 100
+height_min = 100
+width_min = 100
+depth_min = 100
+
+length_max = 1000
+height_max = 1000
+width_max = 1000
+depth_max = 1000
+
 # Handler of HTTP requests / responses
 
 
@@ -27,6 +37,21 @@ def update_defaults(filename, tmp_filename, params):
         # name="cdepth" value="123456"><br>
         replace = 'name="' + param[0] + '"' + ' value="' + param[1] + '"'
         data = re.sub(find, replace, data)
+    file = open(tmp_filename, "wt")
+    file.write(data)
+    file.close()
+
+
+def set_error_message(min, max, variable, filename, tmp_filename):
+      # Sets the default values in html file
+    file = open(filename)
+    data = file.read()
+    file.close()
+    find = 'name="' + variable[0] + '"' + ' value=".*?">'
+    # name="cdepth" value="123456"><br>
+    replace = 'name="' + str(variable[0]) + '"' + ' value="' + \
+        variable[1] + '">' + "Error. Value is out of bounds."
+    data = re.sub(find, replace, data)
     file = open(tmp_filename, "wt")
     file.write(data)
     file.close()
@@ -65,11 +90,23 @@ class MyHandler(BaseHTTPRequestHandler):
             param_line = post_body.decode()
             params = parse_parameters(param_line)
 
-            # html interface to show
-            update_defaults("userinterface.html",
-                            "userinterface_tmp.html",
-                            params)
-            self.write_HTML_file("userinterface_tmp.html")
+            # for i in range(len(params)):
+            #params[i][1] = float(params[i][1])
+
+            length = float(params[0][1])
+            height = params[1][1]
+            width = params[2][1]
+            depth = params[3][1]
+
+            if length_min > length or length_max < length:
+                set_error_message(
+                    length_min, length_max, params[0], "userinterface.html", "userinterfacetmp2.html")
+                self.write_HTML_file("userinterfacetmp2.html")
+            # # html interface to show
+            # update_defaults("userinterface.html",
+            #                 "userinterface_tmp.html",
+            #                 params)
+            # self.write_HTML_file("userinterface_tmp.html")
 
 
 if __name__ == '__main__':
