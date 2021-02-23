a6909526162b2a2ea13b87bdd71eaab49b45dba5
Oscar Petlund <opetl@hotmail.com>
2021-02-23 13:41:57 +0100

    Writing user values to dfa file added. dfa template file created

--- clientServer.py
+++ clientServer.py
@@ -7,22 +7,37 @@ PORT_NUMBER = 1234
 # Web servers example: http://www.ntnu.edu:80
 
 
-variable_range = [
-    # [name, min, max]
-    ['clength', 0, 1000],
-    ['cheight', 0, 2000],
-    ['cwidth',  0, 500],
-    ['cdepth',  0, 500]
-]
-variable_range2 = {
+variable_range = {
     "seat_depth": [0, 10],
     "chair_width": [0, 10],
     "back_height": [0, 10],
     "leg_side": [0, 10],
     "seat_height": [0, 10],
 }
+variable_to_DFA = {
+    "seat_depth": "PARAM_SEAT_DEPTH",
+    "chair_width": "PARAM_WIDTH",
+    "back_height": "PARAM_BACK_HEIGHT",
+    "leg_side": "PARAM_FRAME_THICKNESS",
+    "seat_height": "PARAM_LEG_HEIGHT",
+}
 
-# Handler of HTTP requests / responses
+
+def create_DFA(params, dfa_filename, dfa_template):
+    # Open dfa template
+    file = open(dfa_template)
+    dfa_data = file.read()
+    file.close()
+
+    # Insert parameter values
+    for param_pair in params:
+        find = "<" + variable_to_DFA[param_pair[0]] + ">"
+        dfa_data = dfa_data.replace(find, param_pair[1])
+
+    # Write to new dfa file
+    file = open(dfa_filename, "wt")
+    file.write(dfa_data)
+    file.close()
 
 
 def parse_parameters(string):
@@ -67,6 +82,8 @@ def set_error_message(min, max, variable, error_filename):
     file.write(data)
     file.close()
 
+# Handler of HTTP requests / responses
+
 
 class MyHandler(BaseHTTPRequestHandler):
 
@@ -76,15 +93,16 @@ class MyHandler(BaseHTTPRequestHandler):
             file = open(filename).read()
             self.send_response(200)
             self.send_header("Content-type", "text/html")
+            self.end_headers()
         except:
-            file = "Error. File not found."
-            self.send_response(404)
-        self.end_headers()
+            file = "Error. The file " + filename + " does not exist."
         self.wfile.write(bytes(file, 'utf-8'))
 
     def do_GET(self):
-        if (self.path == '/') or (self.path.find("/orderChair") != -1) or (self.path.find("/userinterface.html") != -1):
-            self.path = "/orderChair/userinterface.html"
+        if (self.path == '/') or (self.path.find("/orderChair") != -1):
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
             self.write_HTML_file("userinterface.html")
 
     def do_POST(self):
@@ -99,32 +117,26 @@ class MyHandler(BaseHTTPRequestHandler):
             post_body = self.rfile.read(content_len)
             param_line = post_body.decode()
             params = parse_parameters(param_line)
-            print(params)
 
+            # Update values in html files from param
             update_defaults("userinterface.html",
                             "userinterface_tmp.html",
                             "userinterface_error.html",
                             params)
 
-            # for i in range(len(params)):
-            #params[i][1] = float(params[i][1])
-
-            length = float(params[0][1])
-            height = float(params[1][1])
-            width = float(params[2][1])
-            depth = float(params[3][1])
-
+            # Check for illegal parameter
             illegal_value = False
 
-            for i, param in enumerate(params):
-                value = float(param[1])
-                min_value = variable_range[i][1]
-                max_value = variable_range[i][2]
+            for param_pair in params:
+                param = param_pair[0]
+                value = float(param_pair[1])
+                min_value = variable_range[param][0]
+                max_value = variable_range[param][1]
                 if value < min_value or value > max_value:
                     set_error_message(
                         min_value,
                         max_value,
-                        param,
+                        param_pair,
                         "userinterface_error.html")
                     illegal_value = True
 
@@ -133,6 +145,10 @@ class MyHandler(BaseHTTPRequestHandler):
                 self.write_HTML_file("userinterface_error.html")
             else:
                 self.write_HTML_file("userinterface_tmp.html")
+
+            if not illegal_value:
+                create_DFA(params, "user_chair.dfa",
+                           "my_chair_final_template.dfa")
 
 
 if __name__ == '__main__':
